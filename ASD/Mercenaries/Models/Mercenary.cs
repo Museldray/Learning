a15eaa7cd2adeb090a6f632382efe0f91aaf088d
Museldray <dawid.stuczyk@wp.pl>
2021-03-18 18:45:03 +0100

    Update 18.03.2021

---
+++ ASD/Mercenaries/Models/Mercenary.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mercenaries.Models
+{
+    class Mercenary
+    {
+        private int id = 0;
+        public int posilek;
+        public int sila;
+        public int rozrywka;
+        public Mercenary(int posilek, int sila, int rozrywka)
+        {
+            this.id = id;
+            this.posilek = posilek;
+            this.sila = sila;
+            this.rozrywka = rozrywka;
+
+        }
+        public int Id
+        {
+            get => id;
+            set => id++;
+        }
+    }
+}
--- ASD/Mercenaries/Program.cs
+++ ASD/Mercenaries/Program.cs
@@ -2,20 +2,23 @@
 using System.IO;
 using System.Diagnostics;
 using System.Linq;
+using Mercenaries.Models;
+using System.Collections.Generic;
 
-namespace Algorytm3._3
+namespace Mercenaries
 {
+    // Program to find best army with specific amount of food and "joy"
     class Program
     {
         static int[] wynik;
         static int wynik_sila = 0;
-        public static void znajdzOdpowiedz(Najemnik[] najemnicy, int zywnosc, int rozrywka)
+        public static void znajdzOdpowiedz(List<Mercenary> najemnicy, int zywnosc, int rozrywka)
         {
-            wynik = new int[najemnicy.Length];
+            wynik = new int[najemnicy.Count];
             int p = 0;
-            int[,,] tablica = new int[najemnicy.Length + 1, zywnosc + 1, rozrywka + 1];
+            int[,,] tablica = new int[najemnicy.Count + 1, zywnosc + 1, rozrywka + 1];
 
-            for (int i = 0; i < najemnicy.Length + 1; i++)
+            for (int i = 0; i < najemnicy.Count + 1; i++)
             {
                 tablica[i, 0, 0] = 0;
             }
@@ -30,26 +33,26 @@ namespace Algorytm3._3
                 tablica[0, 0, i] = 0;
             }
 
-            for (int j = 1; j < najemnicy.Length + 1; j++)
+            for (int j = 1; j < najemnicy.Count + 1; j++)
             {
                 for (int i = 1; i < zywnosc + 1; i++)
                 {
                     for(int z = 1; z < rozrywka + 1; z++)
                     {
-                        if (i < najemnicy[j - 1].Posilek || z < najemnicy[j - 1].Rozrywka)
+                        if (i < najemnicy[j - 1].posilek || z < najemnicy[j - 1].rozrywka)
                         {
                             tablica[j, i, z] = tablica[j - 1, i, z];
                         }
                         else
                         {
-                            tablica[j, i, z] = Math.Max(tablica[j - 1, i - najemnicy[j - 1].Posilek, z - najemnicy[j-1].Rozrywka] + najemnicy[j - 1].Sila, tablica[j - 1, i, z]);
+                            tablica[j, i, z] = Math.Max(tablica[j - 1, i - najemnicy[j - 1].posilek, z - najemnicy[j-1].rozrywka] + najemnicy[j - 1].sila, tablica[j - 1, i, z]);
                         }
                     }   
                 }
             }
 
-            wynik_sila = tablica[najemnicy.Length, zywnosc, rozrywka];
-            int numer_najemnika = najemnicy.Length;
+            wynik_sila = tablica[najemnicy.Count, zywnosc, rozrywka];
+            int numer_najemnika = najemnicy.Count;
             int azywnosc = zywnosc;
             int arozrywka = rozrywka;
             int x = tablica[numer_najemnika, azywnosc, arozrywka];
@@ -59,8 +62,8 @@ namespace Algorytm3._3
                 {
                     wynik[p] = numer_najemnika;
                     p++;
-                    azywnosc -= najemnicy[numer_najemnika-1].Posilek;
-                    arozrywka -= najemnicy[numer_najemnika-1].Rozrywka;
+                    azywnosc -= najemnicy[numer_najemnika-1].posilek;
+                    arozrywka -= najemnicy[numer_najemnika-1].rozrywka;
                     numer_najemnika--;
                     x = tablica[numer_najemnika, azywnosc, arozrywka];
                 }
@@ -74,25 +77,34 @@ namespace Algorytm3._3
 
         static void Main()
         {          
-            var plik_in = File.ReadLines("in.txt");           
-            string[] linia1 = plik_in.First().Split(' ');
-            int ilosc_wojakow = Int32.Parse(plik_in.Skip(1).Take(1).First());
-            int zywnosc = Int32.Parse(linia1[0]);
-            int rozrywka = Int32.Parse(linia1[1]);
-            Najemnik[] najemnicy = new Najemnik[ilosc_wojakow];
-            Stopwatch stopWatch = new Stopwatch();                                          //TIMER START
+            var plik_in = File.ReadLines("in.txt");
+
+            string[] resources = plik_in.First().Split(' ');
+
+            int zywnosc = Int32.Parse(resources[0]);
+            int rozrywka = Int32.Parse(resources[1]);
+
+            List<Mercenary> najemnicy = new List<Mercenary>();
+
+            // Timer start
+            Stopwatch stopWatch = new Stopwatch();
             stopWatch.Start();
-            for(int i = 0; i < ilosc_wojakow; i++)                                          //TWORZENIE NAJEMNIKÓW I WPISYWANIE DO TABLICY
+
+            // Load data about available mercenaries from file 
+            foreach(string line in plik_in.Skip(2))
             {
-                string[] linia = plik_in.Skip(i + 2).Take(1).First().Split(' ');
-                najemnicy[i] = new Najemnik(i, Int32.Parse(linia[1]), Int32.Parse(linia[0]), Int32.Parse(linia[2]));
-                //Console.WriteLine("Id = " + najemnicy[i].Id + " Sila = " + najemnicy[i].Sila + " Cena = " + najemnicy[i].Posilek + " Długość tablicy = " + najemnicy.Length + " Rozrywka = " + najemnicy[i].Rozrywka);   //DO TESTOW
+                string[] details = line.Split(' ');
+                najemnicy.Add(new Mercenary(Int32.Parse(details[1]), Int32.Parse(details[0]), Int32.Parse(details[2])));
             }
-            znajdzOdpowiedz(najemnicy, zywnosc, rozrywka);                                  //WYWOŁANIE FUNKCJI
-            stopWatch.Stop();                                                               //TIMER STOP
+
+            znajdzOdpowiedz(najemnicy, zywnosc, rozrywka);
+
+            // Stop timer and format elapsed time
+            stopWatch.Stop();
             TimeSpan ts = stopWatch.Elapsed;                                                
             string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
 
+            // Save results to file
             using (StreamWriter plik_out = new StreamWriter("out.txt"))
             {
                 plik_out.WriteLine(wynik_sila);
@@ -100,20 +112,22 @@ namespace Algorytm3._3
                 {
                     if(wynik[i] != 0)
                     {
-                        plik_out.Write(wynik[i] + " ");                                      //ZAPIS DO PLIKU
+                        plik_out.Write(wynik[i] + " ");
                     }                                  
                 }
                 plik_out.WriteLine("\n" + elapsedTime);
             }
 
+            // Faster tests
             Console.WriteLine(wynik_sila);
             for (int i = wynik.Length - 1; i>=0; i--)
             {
                 if (wynik[i] != 0)
-                {                                                                             //SZYBSZE TESTY
+                {                                                                             
                     Console.Write(wynik[i] + " ");
                 }                                  
             }
+
             Console.WriteLine("\n" + elapsedTime);
             Console.ReadKey();
         }
