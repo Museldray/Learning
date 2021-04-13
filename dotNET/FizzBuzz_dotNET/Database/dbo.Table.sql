589729478fc41d1106508a210f36947099e2db8b
Museldray <dawid.stuczyk@wp.pl>
2021-04-13 08:34:45 +0200

    Had some problems so rollback master to vanilla FizzBuzz

--- dotNET/FizzBuzz_dotNET/Database/dbo.Table.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[FizzBuzz]
-(
-	[Id]    INT        IDENTITY (1, 1) NOT NULL,
-	[Number]	INT		NOT NULL,
-    [Result]	VARCHAR (50)	NOT NULL,
-    [Date]	datetime     NOT NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-)
