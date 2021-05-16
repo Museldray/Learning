d3686b5f0cc3467baf3ddd8597777fcae9e32d9d
Museldray <dawid.stuczyk@wp.pl>
2021-05-16 13:20:26 +0200

    dotNET_Filtry added

---
+++ dotNET/dotNET_filtry/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotNET_filtry.Utils;
+
+namespace dotNET_filtry.Pages
+{
+    [CustomFilterAttributes]
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ dotNET/dotNET_filtry/Utils/CustomFilterAttributes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace dotNET_filtry.Utils
+{
+    public class CustomFilterAttributes : ResultFilterAttribute
+    {
+        public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+
+            var ipAddress = context.HttpContext.Connection.RemoteIpAddress.ToString();
+            var result = (PageResult)context.Result;
+            result.ViewData["IPAddress"] = ipAddress;
+
+            await next.Invoke();
+            //base.OnResultExecuting(context);
+        }
+    }
+}
